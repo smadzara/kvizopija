b2adf62b30ee3511798ee46d7f0696abf5e67b43
smadzara <sasa.madzarac@gmail.com>
2022-09-05 11:05:09 +0200

    navigacija

--- footer.php
+++ footer.php
@@ -21,6 +21,8 @@
 	</footer><!-- #colophon -->
 </div><!-- #page -->
 
+<!-- <script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script> -->
+
 <?php wp_footer(); ?>
 
 </body>
